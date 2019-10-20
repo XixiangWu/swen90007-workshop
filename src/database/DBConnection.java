7f2b0d200808e271e81eee152e76162f7e86d7d3
xixiangw@student.unimelb.edu.au <xixiangw@student.unimelb.edu.au>
2019-10-21 04:05:26 +1100

    Update DBConnection.java

--- src/database/DBConnection.java
+++ src/database/DBConnection.java
@@ -13,10 +13,10 @@ public class DBConnection {
 
 
     // Database credentials
-//    private static final String DB_USER = "aopdyzhcotqqme";
-//    private static final String DB_PASSWORD = "ae5f296058696c800bef11a36bf53752bf2169dfc9f1ca5c9b3c56f2520ef6b5";
-    private static final String DB_USER = "postgres";
-    private static final String DB_PASSWORD = "xindefengbao12";
+    private static final String DB_USER = "aopdyzhcotqqme";
+    private static final String DB_PASSWORD = "ae5f296058696c800bef11a36bf53752bf2169dfc9f1ca5c9b3c56f2520ef6b5";
+//    private static final String DB_USER = "postgres";
+//    private static final String DB_PASSWORD = "xindefengbao12";
 
     private static Connection dbConnection = null;
 
