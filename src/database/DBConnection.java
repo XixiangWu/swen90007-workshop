68d66b7b5bdb435fd840000135314de85248a488
xixiangw@student.unimelb.edu.au <xixiangw@student.unimelb.edu.au>
2019-10-21 04:05:16 +1100

    Workshop 4 update

--- src/database/DBConnection.java
+++ src/database/DBConnection.java
@@ -8,11 +8,15 @@ import java.sql.SQLException;
 public class DBConnection {
 
     // JDBC driver name and database URL
-    private static final String DB_CONNECTION = "jdbc:postgresql://localhost:5432/bookshop";
+//    private static final String DB_CONNECTION = "jdbc:postgresql://localhost:5432/bookshop";
+    private static final String DB_CONNECTION = "postgres://aopdyzhcotqqme:ae5f296058696c800bef11a36bf53752bf2169dfc9f1ca5c9b3c56f2520ef6b5@ec2-174-129-43-40.compute-1.amazonaws.com:5432/dcmimk7i0tg061";
+
 
     // Database credentials
-    private static final String DB_USER = "aopdyzhcotqqme";
-    private static final String DB_PASSWORD = "ae5f296058696c800bef11a36bf53752bf2169dfc9f1ca5c9b3c56f2520ef6b5";
+//    private static final String DB_USER = "aopdyzhcotqqme";
+//    private static final String DB_PASSWORD = "ae5f296058696c800bef11a36bf53752bf2169dfc9f1ca5c9b3c56f2520ef6b5";
+    private static final String DB_USER = "postgres";
+    private static final String DB_PASSWORD = "xindefengbao12";
 
     private static Connection dbConnection = null;
 
